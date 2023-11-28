e317d5de78dc30ce353dc685ec228468a284a2cd
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2023-11-28 10:08:39 -0700

    jwt and make admin route is complete

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app= express();
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const stripe = require('stripe')(process.env.STRIPE_KEY)
 const port = process.env.PORT || 5000;
 
@@ -30,6 +31,47 @@ async function run() {
     const userCollection = client.db('asset-management').collection('users')
     const hrAssetCollection = client.db('asset-management').collection('hrasset')
 
+
+    // JWT RELATED
+    app.post('/jwt', async(req,res)=>{
+        const user= req.body;
+        const token= jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,
+            {expiresIn: '1d'} );
+            res.send({token})
+    })
+//    middlewares
+const verifyToken =(req,res,next)=>{
+     console.log('inside verify token', req.headers);
+     if(!req.headers.authorization){
+        return res.status(401).send({message: 'Unathorized access'})
+    }
+    const token= req.headers.authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+       if(err){
+       return res.status(401).send({message: 'Unathorized access'})
+       }
+       req.decoded = decoded;
+       next()        
+    })
+    //  next()
+}
+ const verifyAdmin = async (req,res,next)=>{
+    const email = req.decoded.email;
+    console.log(req.decoded.email)
+
+    const query={ email: email }
+    const user = await userCollection.findOne(query)
+    const isAdmin = user?.role === 'admin'
+    if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'})
+    }
+    next()
+}
+
+
+
+
+
 // user create
     app.post('/users', async (req,res)=>{
         const user = req.body;
@@ -42,44 +84,80 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/users', async(req,res)=>{
+       
+        const email = req.query.email;
+        const query ={email: email}
+       const result = await userCollection.find(query).toArray()
+       res.send(result)
+    })
+
+    // ------------------------
+    //  admin
+    // ----------------
+    
+    
+    // ***admin cheak
+    app.get('/users/admin/:email', verifyToken, async(req,res)=>{
+        const email = req.params.email;
+
+        if(email !== req.decoded.email){
+         return res.status(403).send({message: 'Forbidden access'})
+        }
+        const query = {email: email}
+
+        const user = await userCollection.findOne(query)
+        let admin = false
+        if(user){
+            admin = user?.role === 'admin'
+        }
+        res.send({admin})
+      })
     // added product by admin
     // TODO: jwt empliment
-    app.post('/addProduct', async(req,res)=>{
+    app.post('/addProduct',verifyToken, async(req,res)=>{
         const product = req.body;
         const result = await hrAssetCollection.insertOne(product)
         res.send(result)
     })
-    app.get('/addProduct', async(req,res)=>{
+    // ------------------
+
+    
+    app.get('/addProduct',verifyToken,verifyAdmin,async(req,res)=>{
+        // console.log(req.headers)
+
         const email = req.query.email;
         const query = {email: email}
         const result = await hrAssetCollection.find(query).toArray()
         res.send(result)
     })
-    app.delete("/addProduct/:id", async(req,res)=>{
+    app.delete("/addProduct/:id",verifyToken,verifyAdmin, async(req,res)=>{
         const id =req.params.id;
         const query= {_id: new ObjectId(id)}
         const result = await hrAssetCollection.deleteOne(query)
         res.send(result)
      })
+// --------------------------------
+//             payments by stripe
+// ---------------------------------------
+app.post('/create-payment-intent', async(req,res)=>{
+    const {package}= req.body;
+    // console.log(package)
+    const price = parseInt(package )*100
+    // const price = 150
+        console.log(price)
+    const paymentIntent = await stripe.paymentIntents.create({
+       amount: price,
+       currency: 'usd',
+       payment_method_types: [
+           "card"
+         ],
+    })
 
-// payments by stripe
-// app.post('/create-payment-intent', async(req,res)=>{
-//     const {package}= req.body;
-//     console.log(package)
-//     const price = parseFloat(package)
-
-//     const paymentIntent = await stripe.paymentIntents.create({
-//        amount: price,
-//        currency: 'usd',
-//        payment_method_types: [
-//            "card"
-//          ],
-//     })
-
-//     res.send({
-//         clientSecret: paymentIntent.client_secret
-//     })
-// })
+    res.send({
+        clientSecret: paymentIntent.client_secret
+    })
+})
 
 
 
